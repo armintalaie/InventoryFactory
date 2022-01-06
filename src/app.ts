9e6d3f4dc55a4ee0232d7ecc363bc5676f418c33
armintalaie <talaiearmin78@gmail.com>
2022-01-05 18:00:55 -0800

    CRUD operations for inventory items

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,60 @@
+import express from 'express';
+import { InventoryHandler } from './inventory';
+import { InventoryItem } from './itemHandler';
+
+const app = express();
+app.use(express.json());
+const port = 8080; // default port to listen
+
+let inventory: InventoryHandler;
+inventory = new InventoryHandler();
+
+// define a route handler for the default home page
+app.get('/', (req, res) => {
+    res.send( "HI" );
+} );
+
+// start the Express server
+app.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` );
+});
 
 
-const world = 'world';
+app.post('/createItem', (req, res) => {
+    const title: string = req.body.title as string;
+    const quantity: number = req.body.quantity as number;
 
-export function hello(world: string): void {
-  console.log(`Hello ${world}!`);
-}
+    const ret: InventoryItem = inventory.createItem({ title, quantity });
+    res.send(JSON.stringify(ret));
+})
 
 
-hello(world);
+app.get('/deleteItem/:id', (req, res) => {
+    const id: string = req.params.id as string;
+
+    const ret: boolean = inventory.deleteItem(id);
+    res.send(ret);
+})
+
+app.get('/items', (req, res) => {
+
+    const ret: InventoryItem[] = inventory.getItems();
+    res.send(JSON.stringify(ret));
+})
+
+app.post('/editItem/:id', (req, res) => {
+    const id: string = req.params.id as string;
+    const ret: InventoryItem | undefined = inventory.getItem(id);
+    const title: string = req.body.title as string;
+    const quantity: number = req.body.quantity as number;
+    const result: boolean = inventory.editItem({ id: id, title: title, quantity: quantity });
+    res.send(result);
+}) 
+
+app.get('/getItem/:id', (req, res) => {
+    const id: string = req.params.id as string;
+    const ret: InventoryItem | undefined = inventory.getItem(id);
+    res.send(JSON.stringify(ret));
+
+}) 
+
--- src/itemHandler.ts
+++ src/itemHandler.ts
@@ -1,15 +1,67 @@
+import { v4 as UUID } from 'uuid';
 
 
-function createItem() {
+export type Item =  {
+    id?: string
+    title: string;
+    quantity: number;
+    description?: string;
+};
+
+
+
+export abstract class ItemHandler {
+    private items: InventoryItem[] = [];
+
+    public createItem(item: Item): InventoryItem {
+        const newItem: InventoryItem = new InventoryItem(item);
+        this.items.push(newItem);
+        return newItem;
+    }
+
+    public deleteItem(id: string): boolean {
+        const itemIndex: number = this.items.findIndex(e => e.id === id);
+        if (itemIndex >= 0) {
+            this.items.splice(itemIndex,1);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public getItem(id: string): InventoryItem  | undefined {
+        return this.items.find(e => e.id === id);
+    }
+
+    public editItem(item: Item): boolean{
+        const itemIndex: number = this.items.findIndex(e => e.id === item.id);
+        if (itemIndex >= 0) {
+            this.items[itemIndex].quantity = item.quantity;
+            this.items[itemIndex].title = item.title;
+            return true;
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+    public getItems(): InventoryItem[] {
+        return this.items;
+    }
+
 }
 
-function editItem() {
-}
+export class InventoryItem {
+    public title: string;
+    public description?: string;
+    public quantity: number;
+    public id: string = UUID();
 
-function deleteItem() {
+    constructor(item: Item) {
+        this.title = item.title;
+        this.quantity = item.quantity;
+    }
 
-}
-
-function getItem() {
     
 }
+
